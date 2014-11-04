dd4ec23953ac1d57202df7a21526ba7b22587622
Ethan Barhydt <ethanbarhydt@gmail.com>
2014-11-04 12:58:59 -0500

    fix merge

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
       get :cite
     end
     collection do
-<<<<<<< HEAD
       get :cite_all
-=======
-    	get :cite_all
->>>>>>> cite-refactor
     end
   end
   
