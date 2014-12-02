5412cd93c3be9b7cc9a46f4d6bc50115ce403e77
Ethan Barhydt <ethanbarhydt@gmail.com>
2014-12-02 15:17:57 -0500

    Connect front- and back-ends

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,30 +3,17 @@ class ArticlesController < ApplicationController
   	@articles = Article.all
   end
 
+  
   def design
-    #FOR DESIGN PROTOTYPING
   end
-  def article1
-    @article1 = "pushdown"
-  end
-  def article2
-    @article2 = "pushdown"
-  end
-  def article3
-    @article3 = "pushdown"
-  end
-  def article4
-    @article4 = "pushdown"
-  end
-  def article5
-    @article5 = "pushdown"
-  end
+ 
   def citearticles
   end
 
   def show
     @article = Article.find_by_id(params[:id])
   end
+
 
   def cite
   	article = Article.find_by_id(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
+  
+  root 'articles#design'
+
   resources :articles do
     get :cite, :on => :member
     get :cite_all, :on => :collection
   end
   
   match 'design', to: 'articles#design', via: 'get'
-  match 'article1', to: 'articles#article1', via: 'get'
-  match 'article2', to: 'articles#article2', via: 'get'
-  match 'article3', to: 'articles#article3', via: 'get'
-  match 'article4', to: 'articles#article4', via: 'get'
-  match 'article5', to: 'articles#article5', via: 'get'
-  match 'citearticles', to: 'articles#citearticles', via: 'get'
 
-  root 'articles#article1'
 
 end
