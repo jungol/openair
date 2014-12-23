391c3079e7702b27869baf1f027e5e8b64966910
Ethan Barhydt <ethanbarhydt@gmail.com>
2014-12-23 16:40:01 -0600

    load articles w/ jQuery

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find_by_id(params[:id])
+    respond_to do |format|
+      format.js {}
+      format.html {}
+    end
   end
 
 end
